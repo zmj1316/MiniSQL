033378e5d2759fb10229223bbef3c69528778d1e
zmj1316 <zmj1316@gmail.com>
2015-10-29 23:58:11 +0800

    Signed-off-by: zmj1316 <zmj1316@gmail.com>

--- src/buffer.h
+++ src/buffer.h
@@ -25,4 +25,5 @@ void newBlock(Buffer *);
 /* Private Functions */
 static void diskWrite(Buffer *buf, u32 block, u8 * bin);
 static void diskRead(Buffer *buf, u32 block, u8 * bin);
+static u32 LUR(Buffer *buf, u32 block);
 #endif // _BUFFER_H
